cd3741445650bb3bb0badc186442a60ec0d6db12
Jon Black <jblack@JolleCocoon.local>
2017-01-13 15:21:12 -0500

    able to grab random word from dictionary

--- word.rb
+++ word.rb
@@ -8,5 +8,15 @@ class Word
   def grab_random_word
     #returns a random word from the dictionary or other source
 
+    # rando2 =  File.read('/usr/share/dict/words').lines.select {|l| (5 .. 12).cover?(l.strip.size)}.sample.strip
+    # puts rando2
+    File.read('/usr/share/dict/words').lines.select {|l| (4..9).cover?(l.strip.size)}.sample.strip
+
   end
 end
+
+w = Word.new.grab_random_word
+puts w
+
+
+
